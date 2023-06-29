adbcb3f94534f9a86781616f19a457a9c016877f
Helen Medeiros <116672671+byhelenmedeiros@users.noreply.github.com>
2023-06-29 23:04:10 +0100

    navbar

--- website/src/pages/home/home.js
+++ website/src/pages/home/home.js
@@ -1,13 +1,28 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { Navbar, Container, Nav, Form, FormControl, Button } from 'react-bootstrap';
-import { Search } from 'react-bootstrap-icons'
+import { Navbar, Container, Nav, Form, FormControl } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import './Home.css';
 
 const Home = () => {
   const [showSearch, setShowSearch] = useState(false);
+  const [searchTerm, setSearchTerm] = useState('');
 
   const handleSearchClick = () => {
     setShowSearch(!showSearch);
+    setSearchTerm('');
+  };
+
+  const handleChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const handleKeyPress = (event) => {
+    if (event.key === 'Enter') {
+      event.preventDefault();
+      console.log('Pesquisar:', searchTerm); // Substitua pelo código de pesquisa adequado
+    }
   };
 
   return (
@@ -25,22 +40,30 @@ const Home = () => {
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="ml-auto">
+            <Nav>
               <Nav.Link as={Link} to="/">Home</Nav.Link>
               <Nav.Link as={Link} to="/about">Sobre nós</Nav.Link>
               <Nav.Link as={Link} to="/services">Serviços</Nav.Link>
               <Nav.Link as={Link} to="/products">Produtos</Nav.Link>
               <Nav.Link as={Link} to="/contact">Entre em contato</Nav.Link>
-              <Nav.Link as={Button} variant="light" onClick={handleSearchClick}>
-                <Search />
-              </Nav.Link>
             </Nav>
-            {showSearch && (
-              <Form inline>
-                <FormControl type="text" placeholder="Pesquisar" className="mr-sm-2" />
-                <Button variant="outline-light">Buscar</Button>
-              </Form>
-            )}
+            <Form inline className="d-flex align-items-center">
+              {showSearch && (
+                <FormControl
+                  type="text"
+                  placeholder="Pesquisar"
+                  className="mr-2"
+                  value={searchTerm}
+                  onChange={handleChange}
+                  onKeyPress={handleKeyPress}
+                />
+              )}
+              <FontAwesomeIcon
+                icon={faSearch}
+                className={`search-icon ${showSearch ? 'active' : ''}`}
+                onClick={handleSearchClick}
+              />
+            </Form>
           </Navbar.Collapse>
         </Container>
       </Navbar>
