fcd388a3e155bf76c132162ce2331415aff6b6a5
Helen Medeiros <116672671+byhelenmedeiros@users.noreply.github.com>
2023-06-29 22:33:52 +0100

    npm uninstall react-bootstrap-icons

--- website/src/pages/home/home.js
+++ website/src/pages/home/home.js
@@ -1,15 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import { useState } from 'react';
 import { Navbar, Container, Nav, Form, FormControl, Button } from 'react-bootstrap';
-import { Search } from 'react-bootstrap-icons';
-
-//import logo from './logo.png'; 
+import { Search } from 'react-bootstrap-icons'
 
 const Home = () => {
+  const [showSearch, setShowSearch] = useState(false);
+
+  const handleSearchClick = () => {
+    setShowSearch(!showSearch);
+  };
+
   return (
     <>
       <Navbar bg="primary" variant="dark" expand="lg">
@@ -22,18 +22,25 @@ const Home = () => {
               className="d-inline-block align-top"
               alt="Salomeflorista"
             />
-            
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ml-auto">
               <Nav.Link as={Link} to="/">Home</Nav.Link>
               <Nav.Link as={Link} to="/about">Sobre nós</Nav.Link>
-              <Nav.Link as={Link} to="/services">serviços</Nav.Link>
-              <Nav.Link as={Link} to="/Products">Produtos</Nav.Link>
-              <Nav.Link as={Link} to="/contact">Entre em contacto</Nav.Link>
-
+              <Nav.Link as={Link} to="/services">Serviços</Nav.Link>
+              <Nav.Link as={Link} to="/products">Produtos</Nav.Link>
+              <Nav.Link as={Link} to="/contact">Entre em contato</Nav.Link>
+              <Nav.Link as={Button} variant="light" onClick={handleSearchClick}>
+                <Search />
+              </Nav.Link>
             </Nav>
+            {showSearch && (
+              <Form inline>
+                <FormControl type="text" placeholder="Pesquisar" className="mr-sm-2" />
+                <Button variant="outline-light">Buscar</Button>
+              </Form>
+            )}
           </Navbar.Collapse>
         </Container>
       </Navbar>
