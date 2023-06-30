49cf6f9bc68bfb93dee1d55096074c6dbece9308
Helen Medeiros <116672671+byhelenmedeiros@users.noreply.github.com>
2023-06-30 01:22:56 +0100

    imagens

--- website/src/components/Carousel/Carousel.js
+++ website/src/components/Carousel/Carousel.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import Carousel from 'react-bootstrap/Carousel';
 import './CustomCarousel.css';
-import image1 from './images/image1.jpg';
-import image2 from './images/image2.jpg';
-import image3 from './images/image3.jpg';
+import image1 from './images/image1.png';
+import image2 from './images/image2.png';
+import image3 from './images/image3.png';
 
 function CustomCarousel() {
   return (
